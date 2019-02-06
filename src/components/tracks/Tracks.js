ddd9730933266d2bf1c0b95a9826013337b495fe
BrianArchibald <brianallenarchibald@gmail.com>
2019-02-05 21:09:15 -0600

    added axios, api info

--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,15 +1,16 @@
-import React, { Component } from "react";
-import { Consumer } from "../../context";
+import React, { Component } from 'react';
+import { Consumer } from '../../context';
 
-export default class Tracks extends Component {
+class Tracks extends Component {
   render() {
     return (
       <Consumer>
         {value => {
-          console.log(value);
+          return <h1>Tracks</h1>;
         }}
-        <h1>Tracks</h1>
       </Consumer>
     );
   }
 }
+
+export default Tracks;
