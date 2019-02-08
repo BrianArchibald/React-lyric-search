40d08a0fd068fce0ddaa5f260c0342ce7e78410f
BrianArchibald <brianallenarchibald@gmail.com>
2019-02-07 20:39:59 -0600

    added lyric comp, api working, did routes

---
+++ src/components/tracks/Lyrics.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class Lyrics extends Component {
+  state = {
+    track: {},
+    lyrics: {}
+  };
+
+  componentDidMount() {
+    axios
+      .get(
+        `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${
+          this.props.match.params.id
+        }&apikey=c86d207a713301585d32060f61cfe509`
+
+        // ${
+        //   process.env.REACT_APP_MM_KEY
+        // }`
+      )
+      .then(res => {
+        console.log(res.data);
+        this.setState({ lyrics: res.data.message.body.lyrics });
+
+        return axios.get(
+          `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.get?track_id=${
+            this.props.match.params.id
+          }&apikey=c86d207a713301585d32060f61cfe509`
+
+          // ${
+          //   process.env.REACT_APP_MM_KEY
+          // }`
+        );
+      })
+      .then(res => {
+        console.log(res.data);
+      })
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Lyrics</h1>
+      </div>
+    );
+  }
+}
+
+export default Lyrics;
